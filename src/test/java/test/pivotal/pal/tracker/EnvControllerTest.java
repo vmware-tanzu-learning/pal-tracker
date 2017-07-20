e246778801b23120d90f4c5ac73eb0ec26b79ce4
Tyson Gern <tgern@pivotal.io>
2017-07-20 13:56:50 -0600

    Add tests for deployment lab

---
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -0,0 +1,28 @@
+package test.pivotal.pal.tracker;
+
+import org.junit.Test;
+
+import java.util.Map;
+import io.pivotal.pal.tracker.EnvController;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EnvControllerTest {
+    @Test
+    public void getEnv() throws Exception {
+        EnvController controller = new EnvController(
+            "8675",
+            "12G",
+            "34",
+            "123.sesame.street"
+        );
+
+        Map<String, String> env = controller.getEnv();
+
+        assertThat(env.get("PORT")).isEqualTo("8675");
+        assertThat(env.get("MEMORY_LIMIT")).isEqualTo("12G");
+        assertThat(env.get("CF_INSTANCE_INDEX")).isEqualTo("34");
+        assertThat(env.get("CF_INSTANCE_ADDR")).isEqualTo("123.sesame.street");
+    }
+
+}
