0c3a5f72a323b3e0e0ee405912aad076bcfe3592
Tyson Gern <tgern@pivotal.io>
2017-07-26 12:28:32 -0600

    Add tests for Actuator lab

---
+++ src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
@@ -0,0 +1,38 @@
+package test.pivotal.pal.trackerapi;
+
+import com.jayway.jsonpath.DocumentContext;
+import io.pivotal.pal.tracker.PalTrackerApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.jayway.jsonpath.JsonPath.parse;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PalTrackerApplication.class, webEnvironment = RANDOM_PORT)
+public class HealthApiTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void healthTest() {
+        ResponseEntity<String> response = this.restTemplate.getForEntity("/health", String.class);
+
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext healthJson = parse(response.getBody());
+
+        assertThat(healthJson.read("$.status", String.class)).isEqualTo("UP");
+        assertThat(healthJson.read("$.db.status", String.class)).isEqualTo("UP");
+        assertThat(healthJson.read("$.diskSpace.status", String.class)).isEqualTo("UP");
+    }
+}
