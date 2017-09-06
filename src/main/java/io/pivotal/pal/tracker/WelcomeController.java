cc997cda38cc4362baae2e8294c5325107916cb9
Satish <sarthak.satish@capgemini.com>
2017-09-06 07:34:53 +0530

    adsa

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @Value("${WELCOME_MESSAGE}")
+
     public String message;
 
-    public WelcomeController(String message){
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message){
         this.message=message;
     }
     @GetMapping("/")
