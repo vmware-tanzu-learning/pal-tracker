7b6f3b70cf366526066ca54ffba38942fc8aec74
bhavesh shah <bhavesh027@gmail.com>
2017-11-01 18:12:45 +0530

    conconsoure

--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -9,6 +9,7 @@ public class WelcomeControllerTest {
 
     @Test
     public void itSaysHello() throws Exception {
+
         WelcomeController controller = new WelcomeController("A welcome message");
 
         assertThat(controller.sayHello()).isEqualTo("A welcome message");
