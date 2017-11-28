701542d10578158e690bddd69a2901ba96d36120
Meg Basant <meg.basant@gmail.com>
2017-11-27 17:16:33 -0700

    Simple Spring Boot app

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+
+}
