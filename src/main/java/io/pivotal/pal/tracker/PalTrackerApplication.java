e3dc50adcd1fd003bc4e953c1457dc97b904df54
Magic Frank <frank@somuchmagic.com>
2018-03-26 16:03:00 -0400

    Simple Spring Boot app

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,15 @@
+package io.pivotal.pal.tracker;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by accenturelabs on 3/26/18.
+ */
+@SpringBootApplication
+public class PalTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+}
---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by accenturelabs on 3/26/18.
+ */
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String sayHello() {
+        return "hello";
+    }
+}
