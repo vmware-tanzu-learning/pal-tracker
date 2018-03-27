7f82e2a410ac8bc76e9f59b7ef2ab6c9960d1b03
Magic Frank <frank@somuchmagic.com>
2018-03-27 11:21:48 -0400

    finished building cf application pal-tracker

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    private String message;
 
-    @GetMapping
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+        this.message = message;
+    }
+
+    @GetMapping("/")
     public String sayHello() {
-        return "hello, George";
+        return message;
     }
 }
