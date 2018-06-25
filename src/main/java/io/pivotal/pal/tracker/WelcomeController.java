a855799a4d72b34e5ef09aea09867cbede3fd658
Saioa Picado Fernández <spicadofernndez@stubhub.com>
2018-06-25 11:58:17 +0200

    Add welcome and Env Controller

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,12 +1,19 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+        private String message = "hello";
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+        this.message = message;
+    }
+
     @GetMapping("/")
     public String sayHello(){
-        return "hello";
+        return message;
     }
 }
